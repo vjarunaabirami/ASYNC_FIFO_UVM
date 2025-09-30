eb13ff56f808dd4f8352f5b7c7adfc34edfb5647
Aruna Abirami V J <arunaabirami@feserver.mirafra.com>
2025-09-30 15:58:54 +0530

    added interface

--- src/asyn_fifo_interface.sv
+++ src/asyn_fifo_interface.sv
@@ -1,7 +1,6 @@
 interface async_fifo_interface(input wclk, rclk);
   bit [7:0] wdata;
   bit [7:0] rdata;
-  bit       wclk_en;
   bit       winc;
   bit       rinc;
   bit       wrst_n;
@@ -20,8 +19,34 @@ interface async_fifo_interface(input wclk, rclk);
 
   clocking read_drv_cb @(posedge rclk);
     default input #0 output #0;
-    
-
+    input rempty;
+    input wfull;
+    output rrst_n;
+    output rinc;
+    output rdata; 
   endclocking
 
+  clocking write_mon_cb @(posedge wclk);
+    default input #0 output #0;
+    input wfull;
+    input rempty;
+    input wrstn;
+    input winc;
+    input wdatal;
+  endclocking
 
+  clocking read_mon_cb @(posedge rclk);
+    default input #0 output #0;
+    input wfull;
+    input rempty;
+    input rrstn;
+    input rinc;
+    input rdata;
+  endclocking
+
+  modport WRITE_DRV_MP (clocking write_drv_cb);
+  modport READ_DRV_MP (clocking read_drv_cb);
+  modport WRITE_MON_MP (clocking write_mon_cb);
+  modport READ_MON_MP (clocking read_mon_cb);
+
+endinterface
